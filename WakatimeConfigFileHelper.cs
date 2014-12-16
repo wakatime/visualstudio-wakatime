7dffa3b96c14f4e199aa12bb744187b4acf3aca5
Vivek C <c_vivek@rediffmail.com>
2014-12-16 22:08:02 +0530

    Removing unused references, works for >2012
    Making sure all unused references are removed from the c# code. Tested
    and make sure that it installs fine for 2012.

--- WakatimeConfigFileHelper.cs
+++ WakatimeConfigFileHelper.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.IO;
-using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 
 namespace Wakatime.VSPackageWakaTime
--- WakatimeUtilityManager.cs
+++ WakatimeUtilityManager.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Reflection;
-using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
 namespace Wakatime.VSPackageWakaTime
