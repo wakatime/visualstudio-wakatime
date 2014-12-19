896857e33c09f0bca3ff1414988b8fa10e853587
Alan Hamlett <alan.hamlett@gmail.com>
2014-12-19 01:14:54 -0600

    add back VSPackage

--- Resources.Designer.cs
+++ Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace WakaTime.WakaTime {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WakaTime.WakaTime.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WakaTime.VSPackageWakaTime.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
