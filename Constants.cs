90c2a6989ce96d7716fbbdbbbd697f2cd5f65141
Alan Hamlett <alan.hamlett@gmail.com>
2015-07-01 10:37:24 -0700

    v5.0.4

--- Constants.cs
+++ Constants.cs
@@ -2,7 +2,7 @@
 {
     internal static class WakaTimeConstants
     {
-        internal const string CurrentWakaTimeCliVersion = "4.0.14"; //https://github.com/wakatime/wakatime/blob/master/HISTORY.rst
+        internal const string CurrentWakaTimeCliVersion = "4.1.0"; // https://github.com/wakatime/wakatime/blob/master/HISTORY.rst
         internal const string CliUrl = "https://github.com/wakatime/wakatime/archive/master.zip";
         internal const string PluginName = "visualstudio-wakatime";
         internal const string EditorName = "visualstudio";
