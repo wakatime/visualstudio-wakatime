c3ac6cee1a3567035eaa7b186a42341dc62d85ab
Alan Hamlett <alan.hamlett@gmail.com>
2015-08-23 20:20:27 -0700

    catch exceptions from network request when checking current wakatime cli version

--- WakaTimeConstants.cs
+++ WakaTimeConstants.cs
@@ -16,20 +16,28 @@ namespace WakaTime
         {
             var regex = new Regex(@"(__version_info__ = )(\(( ?\'[0-9]\'\,?){3}\))");
             var client = new WebClient();
-            var about = client.DownloadString("https://raw.githubusercontent.com/wakatime/wakatime/master/wakatime/__about__.py");
-            var match = regex.Match(about);
-
-            if (!match.Success)
+            try
             {
-                Logger.Warning("Couldn't auto resolve wakatime cli version");
+                var about = client.DownloadString("https://raw.githubusercontent.com/wakatime/wakatime/master/wakatime/__about__.py");
+                var match = regex.Match(about);
+
+                if (!match.Success)
+                {
+                    Logger.Warning("Couldn't auto resolve wakatime cli version");
+                    return string.Empty;                
+                }
+
+                var grp1 = match.Groups[2];
+                var regexVersion = new Regex("([0-9])");
+                var match2 = regexVersion.Matches(grp1.Value);
+
+                return string.Format("{0}.{1}.{2}", match2[0].Value, match2[1].Value, match2[2].Value);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Exception when checking current wakatime cli version: ", ex);
                 return string.Empty;                
             }
-
-            var grp1 = match.Groups[2];
-            var regexVersion = new Regex("([0-9])");
-            var match2 = regexVersion.Matches(grp1.Value);
-
-            return string.Format("{0}.{1}.{2}", match2[0].Value, match2[1].Value, match2[2].Value);
         };
 
     }
