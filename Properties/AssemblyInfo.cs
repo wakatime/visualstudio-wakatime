ad7cdf0ff8b72daed2e0926f3840b684d73019e3
Alan Hamlett <alan.hamlett@gmail.com>
2015-11-26 10:48:36 -0800

    v7.0.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WakaTime")]
-[assembly: AssemblyDescription("WakaTime is a productivity & time tracking tool for programmers. Once the WakaTime plugin is installed, you get a dashboard with reports about your programming by time, language, project, and branch.")]
+[assembly: AssemblyDescription("Quantify your coding with automatic time tracking and metrics about your programming.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("WakaTime")]
 [assembly: AssemblyProduct("WakaTime")]
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("6.0.0")]
-[assembly: AssemblyFileVersion("6.0.0")]
+[assembly: AssemblyVersion("7.0.0")]
+[assembly: AssemblyFileVersion("7.0.0")]
 
 [assembly: InternalsVisibleTo("WakaTime_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ed8f681fc55bde81467704eba07c3f705ca21f8e864179766c6a034588cd9c2262faaa5c377f0f4fac40ed2564049adf003f19bc13b6d420a1c4d17d998d743fe3cf3ea06957841e7a423d98ea28dd45c43ca7ce3d0722cd859b56101fc1b24f48afc420168a636e486da71c00e9e0909083772c528cb7ecc18ba6e358f5f99a")]
 [assembly: InternalsVisibleTo("WakaTime_UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ed8f681fc55bde81467704eba07c3f705ca21f8e864179766c6a034588cd9c2262faaa5c377f0f4fac40ed2564049adf003f19bc13b6d420a1c4d17d998d743fe3cf3ea06957841e7a423d98ea28dd45c43ca7ce3d0722cd859b56101fc1b24f48afc420168a636e486da71c00e9e0909083772c528cb7ecc18ba6e358f5f99a")]
