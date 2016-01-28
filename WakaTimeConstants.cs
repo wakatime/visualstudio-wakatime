c26d5ca1ccb02e10195046b931e374ca36a23b27
Cameron WiseOwl <camwiseowl@gmail.com>
2016-01-28 14:45:32 +0000

    Update WakaTimeConstants.cs
    Missing second Regex + added (my bad).

--- WakaTimeConstants.cs
+++ WakaTimeConstants.cs
@@ -39,7 +39,7 @@ namespace WakaTime
                 if (match.Success)
                 {
                     var grp1 = match.Groups[2];
-                    var regexVersion = new Regex("([0-9])");
+                    var regexVersion = new Regex("([0-9]+)");
                     var match2 = regexVersion.Matches(grp1.Value);
                     return string.Format("{0}.{1}.{2}", match2[0].Value, match2[1].Value, match2[2].Value);
                 }
