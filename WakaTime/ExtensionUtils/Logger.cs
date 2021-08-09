e657e3cdc63e57a4cd0d8248996a9683f9f673a0
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2021-08-09 15:21:24 -0300

    revamp the entire project
    - Drop support for versions below 2015.
    - It will correctly initialize asyncronously.
    - Update WakaTime.Shared.ExtensionUtils dependecy to v3.0.0.
    - Fix uninstall process that could not find LICENSE file.

---
+++ WakaTime/ExtensionUtils/Logger.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Globalization;
+using WakaTime.Shared.ExtensionUtils;
+
+namespace WakaTime.ExtensionUtils
+{
+    public class Logger : ILogger
+    {
+        private readonly ConfigFile _config;
+        private IVsOutputWindowPane _wakatimeOutputWindowPane;
+        private IVsOutputWindowPane WakatimeOutputWindowPane =>
+            _wakatimeOutputWindowPane ?? (_wakatimeOutputWindowPane = GetWakatimeOutputWindowPane());
+
+        public Logger()
+        {
+            _config = new ConfigFile();
+        }
+
+        private static IVsOutputWindowPane GetWakatimeOutputWindowPane()
+        {
+            if (!(Package.GetGlobalService(typeof(SVsOutputWindow)) is IVsOutputWindow outputWindow)) return null;
+
+            var outputPaneGuid = new Guid(GuidList.GuidWakatimeOutputPane.ToByteArray());
+
+            outputWindow.CreatePane(ref outputPaneGuid, "WakaTime", 1, 1);
+            outputWindow.GetPane(ref outputPaneGuid, out var windowPane);
+
+            return windowPane;
+        }
+
+        public void Debug(string message)
+        {
+            if (!_config.Debug)
+                return;
+
+            Log(LogLevel.Debug, message);
+        }
+
+        public void Error(string message, Exception ex = null)
+        {
+            var exceptionMessage = $"{message}: {ex}";
+
+            Log(LogLevel.HandledException, exceptionMessage);
+        }
+
+        public void Warning(string message)
+        {
+            Log(LogLevel.Warning, message);
+        }
+
+        public void Info(string message)
+        {
+            Log(LogLevel.Info, message);
+        }
+
+        private void Log(LogLevel level, string message)
+        {
+            var outputWindowPane = WakatimeOutputWindowPane;
+            if (outputWindowPane == null) return;
+
+            var outputMessage =
+                $"[WakaTime {Enum.GetName(level.GetType(), level)} {DateTime.Now.ToString("hh:mm:ss tt", CultureInfo.InvariantCulture)}] {message}{Environment.NewLine}";
+
+            outputWindowPane.OutputString(outputMessage);
+        }
+    }
+}
