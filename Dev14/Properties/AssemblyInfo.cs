482e1f9cb9730207ab02fd7b671a5f9b45704eb9
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2022-12-15 20:41:30 -0300

    v13.0.1

--- Dev14/Properties/AssemblyInfo.cs
+++ Dev14/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("13.0.0")]
-[assembly: AssemblyFileVersion("13.0.0")]
+[assembly: AssemblyVersion("13.0.1")]
+[assembly: AssemblyFileVersion("13.0.1")]
 [assembly: NeutralResourcesLanguage("en-US")]
