7bc609f6d7ec27cd92b26eb8f13b536328ba4250
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2023-10-26 21:23:40 -0300

    Changes for v13.2.0

--- Dev17/Properties/AssemblyInfo.cs
+++ Dev17/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("13.0.2")]
-[assembly: AssemblyFileVersion("13.0.2")]
+[assembly: AssemblyVersion("13.2.0")]
+[assembly: AssemblyFileVersion("13.2.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
