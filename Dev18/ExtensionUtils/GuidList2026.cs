6c13e66717c007fa28f19502fc99eab0775b8f76
Alan Hamlett <alan.hamlett@gmail.com>
2026-02-24 12:54:01 -0400

    Stop building separate release for Dev18

--- Dev18/ExtensionUtils/GuidList2026.cs
+++
@@ -1,7 +0,0 @@
-namespace WakaTime.ExtensionUtils
-{
-    public static class GuidList2026
-    {
-        public const string GuidWakaTimePkgString = "E7851384-C3A7-4F70-B4EF-C9A2DCB9794C";
-    }
-}
