0dcb2fcd897b0fd17952a0920a2e4afc90062030
Alan Hamlett <alan.hamlett@gmail.com>
2026-02-24 12:15:59 -0400

    Build for Dev18 Visual Studio 2026

---
+++ Dev18/Forms/SettingsForm.Designer.cs
@@ -0,0 +1,172 @@
+namespace WakaTime.Forms
+{
+    partial class SettingsForm
+    {
+
+        private System.Windows.Forms.Label lblAPIKey;
+        private System.Windows.Forms.TextBox txtAPIKey;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
+            this.lblAPIKey = new System.Windows.Forms.Label();
+            this.txtAPIKey = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtProxy = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkDebugMode = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            //
+            // lblAPIKey
+            //
+            this.lblAPIKey.AutoSize = true;
+            this.lblAPIKey.Location = new System.Drawing.Point(162, 40);
+            this.lblAPIKey.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblAPIKey.Name = "lblAPIKey";
+            this.lblAPIKey.Size = new System.Drawing.Size(133, 41);
+            this.lblAPIKey.TabIndex = 0;
+            this.lblAPIKey.Text = "API Key :";
+            //
+            // txtAPIKey
+            //
+            this.txtAPIKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAPIKey.Location = new System.Drawing.Point(310, 32);
+            this.txtAPIKey.Margin = new System.Windows.Forms.Padding(8);
+            this.txtAPIKey.MaxLength = 41;
+            this.txtAPIKey.Name = "txtAPIKey";
+            this.txtAPIKey.Size = new System.Drawing.Size(1213, 47);
+            this.txtAPIKey.TabIndex = 1;
+            //
+            // btnOk
+            //
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(310, 353);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(8);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(218, 68);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "Save";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.BtnOk_Click);
+            //
+            // btnCancel
+            //
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(585, 353);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(8);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(218, 68);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            //
+            // txtProxy
+            //
+            this.txtProxy.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtProxy.Location = new System.Drawing.Point(310, 125);
+            this.txtProxy.Margin = new System.Windows.Forms.Padding(8);
+            this.txtProxy.MaxLength = 255;
+            this.txtProxy.Name = "txtProxy";
+            this.txtProxy.Size = new System.Drawing.Size(1213, 47);
+            this.txtProxy.TabIndex = 5;
+            //
+            // label1
+            //
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 132);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 41);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Proxy (optional) :";
+            //
+            // chkDebugMode
+            //
+            this.chkDebugMode.AutoSize = true;
+            this.chkDebugMode.Location = new System.Drawing.Point(310, 262);
+            this.chkDebugMode.Margin = new System.Windows.Forms.Padding(8);
+            this.chkDebugMode.Name = "chkDebugMode";
+            this.chkDebugMode.Size = new System.Drawing.Size(146, 45);
+            this.chkDebugMode.TabIndex = 6;
+            this.chkDebugMode.Text = "Debug";
+            this.chkDebugMode.UseVisualStyleBackColor = true;
+            //
+            // label2
+            //
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 190);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1236, 41);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Example: https://user:pass@host:port or socks5://user:pass@host:port or domain\\us" +
+    "er:pass.";
+            //
+            // SettingsForm
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(240F, 240F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1562, 451);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chkDebugMode);
+            this.Controls.Add(this.txtProxy);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtAPIKey);
+            this.Controls.Add(this.lblAPIKey);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(8);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SettingsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WakaTime Settings";
+            this.Load += new System.EventHandler(this.SettingsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtProxy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkDebugMode;
+        private System.Windows.Forms.Label label2;
+    }
+}
